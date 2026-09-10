1692d4fecbafa76708a890e59819e8a6bde7cd5a
Liusi_Orel <lyudochkaorel8@gmail.com>
2026-09-10 18:12:25 +0300

    add tasr 1

---
+++ js/task-1.js
@@ -0,0 +1,17 @@
+function isEnoughCapacity(products, containerSize) {
+  let totalProducts = 0;
+
+  const valuesProd = Object.values(products);
+  for (const key of valuesProd) {
+    return (totalProducts += products[key]);
+  }
+  return totalProducts <= containerSize ? true : false;
+}
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12));
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14));
+
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7));
